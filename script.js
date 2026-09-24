8e723ad50e8cc086905c1287c12b962ca9e205f1
yumavv0ndervvall-blip <yumavv0ndervvall@gmail.com>
2026-09-24 22:49:49 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   var nav = document.getElementById("nav-placeholder");
   if (nav) {
-    fetch("./nav.html?v=80")
+    fetch("./nav.html?v=83")
       .then(function (res) { return res.text(); })
       .then(function (html) {
         nav.innerHTML = html;
